9e1d30156c0b862c662eb23c00ad9b22b50f44ab
thilakshan2003 <rathibala.thilak@gmail.com>
2025-12-11 12:24:55 +0530

    Performance section

--- macbooksite_darazver/src/app.jsx
+++ macbooksite_darazver/src/app.jsx
@@ -3,6 +3,11 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import Showcase from './components/Showcase'
+import Performance from './components/Performance'
+import Highlights from './components/Highlights'
+import Features from './components/Features'
+import Footer from './components/Footer'
+
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 
@@ -15,6 +20,10 @@ const app = () => {
       <Hero />
       <ProductViewer /> 
       <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   )
 }
